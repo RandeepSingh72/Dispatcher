ac6f14718305edb109ebefe2af32212ebad71842
Randeep Singh <rs513535@gmail.com>
2024-01-26 15:47:51 +0530

    update

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const { default: mongoose } = require('mongoose');
 const Job = require('./models/jobModel');
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://fabulous-cactus-a4134a.netlify.app/');
+  res.header('Access-Control-Allow-Origin', 'https://fabulous-cactus-a4134a.netlify.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
