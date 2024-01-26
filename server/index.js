4b642cd5ecc4736188121d126e7a17ab3ab7d42b
Randeep Singh <rs513535@gmail.com>
2024-01-26 15:37:55 +0530

    change

--- server/index.js
+++ server/index.js
@@ -11,6 +11,13 @@ const Address = require('./models/address');
 const { default: mongoose } = require('mongoose');
 const Job = require('./models/jobModel');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://fabulous-cactus-a4134a.netlify.app/');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(cors());
 app.use(bodyParser.json());
 
